1b77565f7944793cdfdc4c10a33edfcaa264fec6
Gustavo Aparecido <gustavorcdaparecido@gmail.com>
2024-03-20 22:31:38 -0300

    Fazendo o TDE05

--- EstruturaDeDados/src/Aula03/ListaJava.java
+++ EstruturaDeDados/src/Aula03/ListaJava.java
@@ -1,11 +1,12 @@
 package Aula03;
 
+import java.util.Arrays;
+
 public class ListaJava {
 
     private final int LIST_SIZE = 10;
     private int tamanho = 0;
-
-    public Integer[] values = new Integer[LIST_SIZE];
+    private Integer[] values = new Integer[LIST_SIZE];
 
     public void add(Integer value) {
         // TODO validar se a lista já atingiu o limite
@@ -16,42 +17,56 @@ public class ListaJava {
         }
 
         // TODO validar se o valor do objeto é maior que zero
-        if(value > 0) {
+        if(value > 0 && value < 1000) {
             System.out.println("O valor digitado é maior que zero.");
-        } else {
-            System.out.println("O valor digitado é menor que zero.");
-            return;
-        }
-
-        for (int i = 0; i < this.values.length; i++) {
-            if (this.values[i] == null) {
-                System.out.println("Adicionando o " + value);
-                this.values[i] = value;
-                tamanho++;
-                break;
+            for (int i = 0; i < this.values.length; i++) {
+                if (this.values[i] == null) {
+                    System.out.println("Adicionando o " + value);
+                    this.values[i] = value;
+                    tamanho++;
+                    break;
+                }
             }
+        } else {
+            System.out.println("O valor digitado é menor que zero. Não pode ser adicionado.");
+            return;
         }
 
     }
 
     public Integer find(int index) {
         // TODO retorne o item pedido
-        if (index >= 0 && index < 10) {
-            System.out.println(values[index]);
-        } else {
-            System.out.println("Valor inválido.");
+        if (index < LIST_SIZE) {
+            return this.values[index - 1];
         }
-        return null;
+        return -9999;
+//        if (index >= 0 && index < 10) {
+//            System.out.println(values[index]);
+//        } else {
+//            System.out.println("Valor inválido.");
+//        }
+//        return null;
     }
 
     private void aumentarTamanho() {
         // TODO DESAFIO!! Descobrir uma maneira de aumentar o tamanho do array (substituir)
+        this.values = Arrays.copyOf(this.values, LIST_SIZE + 1);
 
     }
 
+//    @Override
+//    public String toString() {
+//        // TODO Crie um método para imprimir
+//        return null;
+//    }
+
+
     @Override
     public String toString() {
-        // TODO Crie um método para imprimir
-        return null;
+        return "ListaJava{" +
+                "LIST_SIZE=" + LIST_SIZE +
+                ", tamanho=" + tamanho +
+                ", values=" + Arrays.toString(values) +
+                '}';
     }
 }
--- EstruturaDeDados/src/Main.java
+++ EstruturaDeDados/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        Produto produto = new Produto("Óleo Coamo", 8.99, 3, true);
+        System.out.println(produto.toString());
+
+        Carro carro = new Carro("Chevrolet", "Astra Preto 2.0", 2009, 29000);
+        carro.mostrarInformacoes();
+
+        System.out.println("Insira o valor de desconto para calcular");
+        double desconto = scanner.nextDouble();
+        carro.aplicarDesconto(desconto);
+
+
+
     }
 }
